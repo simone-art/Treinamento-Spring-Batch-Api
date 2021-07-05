15446011d478abd4e28f25ecd9d1e01533208237
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-07-05 10:06:20 -0300

    First commit

---
+++ demo-spring-batch-api/src/main/java/com/example/demospringbatchapi/DemoSpringBatchApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demospringbatchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBatchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBatchApiApplication.class, args);
+	}
+
+}
